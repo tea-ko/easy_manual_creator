8f72c049ef5ef85c553c9d4369c3d40bd79497da
Takako Sato <tea-ko@live.jp>
2018-09-08 09:25:24 +0000

    トップページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/', to: 'tops#index'
   get 'sessions/new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit]
