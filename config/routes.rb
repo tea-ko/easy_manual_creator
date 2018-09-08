c8617776d1cdb1b73665188ac333e3d56cb1041c
Takako Sato <tea-ko@live.jp>
2018-09-08 15:08:46 +0000

    アソシエーション修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit]
-  resources :contents
-  resources :titles
+  resources :titles do
+    resources :contents
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
