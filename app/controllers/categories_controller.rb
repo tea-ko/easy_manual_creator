70e1bd4574065ab07b46502a3f17693cded1d63a
Takako Sato <tea-ko@live.jp>
2018-09-16 07:03:52 +0000

    バリデーションの追加とデザイン成形

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_action :require_loggin, only: [:new, :edit, :create, :update, :destroy]
 
   def index
     @categories = Category.all
@@ -60,5 +61,11 @@ class CategoriesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def category_params
       params.require(:category).permit(:id, :name)
+    end
+    
+    def require_loggin
+      unless logged_in? then
+        render new_session_path
+      end
     end
 end
