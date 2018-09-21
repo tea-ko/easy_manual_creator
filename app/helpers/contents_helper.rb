ee4433567d301a86ff21b19b099cdcd197a27863
Takako Sato <tea-ko@live.jp>
2018-09-21 15:31:27 +0000

    シンボル表記に修正

--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -1,8 +1,8 @@
 module ContentsHelper
   def choose_new_or_edit
-    if action_name == 'new' || action_name == 'confirm'
+    if action_name == :new || action_name == :confirm
       confirm_contents_path
-    elsif action_name == 'edit'
+    elsif action_name == :edit
       content_path
     end
   end
--- app/models/title.rb
+++ app/models/title.rb
@@ -1,6 +1,6 @@
 class Title < ApplicationRecord
    has_many :contents, dependent: :destroy
-   #belongs_to :user
    belongs_to :category
+   #belongs_to :user
    validates :title, presence: true
 end
